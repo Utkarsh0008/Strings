d79f505399e39f1783f3540881ff4eafe72231ca
Utkarsh0008 <108312829+Utkarsh0008@users.noreply.github.com>
2022-07-31 06:09:50 +0530

    Add files via upload

---
+++ String_string_compression.java
@@ -0,0 +1,54 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class String_string_compression {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str=sc.next();
+		
+		compress(str);
+
+	}
+
+	private static void compress(String str) {
+		int count=1;
+		String ans="";
+		char ch=str.charAt(0);
+		
+		if(str.length()==1) {
+			System.out.print(ans+ch);
+		}
+		for(int i=1;i<str.length();i++) {
+			
+			
+			if(str.charAt(i)==str.charAt(i-1)) {
+				count++;
+				
+			}
+			
+			else if(count>1) {
+				System.out.print(ans+str.charAt(i-1)+count);
+				count=1;
+			}
+			else{
+				System.out.print(ans+str.charAt(i-1));
+			}
+			if(i==str.length()-1) {
+				if (count>1){
+					System.out.print(ans+str.charAt(i)+count);
+				}
+				else{
+					System.out.println(ans+str.charAt(i));
+				}
+				
+			}
+			
+		
+		}
+		
+		
+	}
+
+}
